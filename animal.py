db3009e8829ff04fa984874b3b15f512d5a44ba9
ParthHogale77 <parthhoglae@gmail.com>
2026-02-04 13:08:17 +0530

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,14 @@
+class Animal:
+    def sound(self):
+        return"some generic sound"
+    
+class Dog(Animal):
+    def sound(self):
+        return "bark"
+    
+class Cat(Animal):
+    def sound(self):
+    
+animals = [Dog(), Cat(), Animal()]
+for animal in animals:
+    print(animal.sound())
